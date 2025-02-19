6dfae8c650c66d13c365b3dfc833f24ca0976f0b
andy duong <andyduong510@gmail.com>
2025-02-18 20:50:41 -0800

    footer textcolor

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="w-full py-8 px-6">
       <div className="mx-auto flex justify-center">
-        <p className="text-sm text-black select-none">© 2025 | Andy Duong</p>
+        <p className="text-sm text-[#aaa] select-none">© 2025 | Andy Duong</p>
       </div>
     </footer>
   );
