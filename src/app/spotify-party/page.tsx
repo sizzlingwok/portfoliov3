f4e9b028d01c8366274b898c08e0dfe3dc92f43b
andy duong <andyduong510@gmail.com>
2025-02-20 22:47:19 -0800

    project page responsive design

--- src/app/spotify-party/page.tsx
+++ src/app/spotify-party/page.tsx
@@ -27,9 +27,24 @@ const marquee = [
 export default function SpotifyParty() {
   return (
     <main className="flex-col min-h-screen">
-      <section className="landing px-24 pt-64 flex flex-col gap-40 select-none">
+      <div className="fixed bottom-12 left-1/2 -translate-x-1/2 z-50">
+        <a
+          href="https://www.figma.com/proto/5ciCRD3R2H8jz0fRfKJPJD/Spotify-Party-Landing-Page?page-id=0%3A1&type=design&node-id=1-2&viewport=1334%2C874%2C0.71&t=MT5QwGlDdOlSKv5J-1&scaling=scale-down"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-sand px-6 py-3 rounded-lg flex items-center gap-2 whitespace-nowrap group transition-all duration-500 ease-in-out lg:hover:ring-2 lg:hover:ring-black/10 lg:hover:shadow-[inset_0_-2px_0_0_rgba(0,0,0,0.05)]"
+        >
+          <span className="flex items-center gap-1">
+            View Prototype
+            <span className="transition-transform duration-500 group-hover:translate-x-1 group-hover:-translate-y-1">
+              ↗
+            </span>
+          </span>
+        </a>
+      </div>
+      <section className="landing px-8 pt-64 flex flex-col gap-40 select-none lg:px-24 transition-[padding] duration-300 ease-in-out">
         <h1 className="text-8xl font-medium">Spotify Party</h1>
-        <div className="flex justify-between gap-4 3xl:text-xl">
+        <div className="flex flex-col justify-between gap-4 3xl:text-xl md:flex-row">
           <p className="font-medium shrink-0 text-xl">001</p>
           <div className="flex flex-col gap-4">
             <div className="flex flex-col gap-2 shrink-0">
@@ -60,25 +75,10 @@ export default function SpotifyParty() {
             also introduce captivating new features to foster an immersive and
             enjoyable journey of music exploration.
           </h2>
-          <div className="shrink-0 flex items-start">
-            <a
-              href="https://www.figma.com/proto/5ciCRD3R2H8jz0fRfKJPJD/Spotify-Party-Landing-Page?page-id=0%3A1&type=design&node-id=1-2&viewport=1334%2C874%2C0.71&t=MT5QwGlDdOlSKv5J-1&scaling=scale-down"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="bg-sand px-6 py-3 rounded-lg flex items-center gap-2 whitespace-nowrap group transition-all duration-500 ease-in-out lg:hover:ring-2 lg:hover:ring-black/10 lg:hover:shadow-[inset_0_-2px_0_0_rgba(0,0,0,0.05)]"
-            >
-              <span className="flex items-center gap-1">
-                View Prototype
-                <span className="transition-transform duration-500 group-hover:translate-x-1 group-hover:-translate-y-1">
-                  ↗
-                </span>
-              </span>
-            </a>
-          </div>
         </div>
       </section>
-      <section className="overflow-hidden py-20 relative bg-white">
-        <Marquee speed={80} gradient={false} direction="left">
+      <section className="overflow-hidden py-20 relative">
+        <Marquee speed={80} direction="left">
           {marquee.map((image, index) => (
             <div
               key={index}
@@ -99,6 +99,16 @@ export default function SpotifyParty() {
             </div>
           ))}
         </Marquee>
+      </section>
+      <section className="landing px-8 pt-64 flex flex-col gap-40 select-none lg:px-24 transition-[padding] duration-300 ease-in-out">
+        <div className="flex flex-col justify-between gap-4 3xl:text-xl md:flex-row">
+          <p className="font-medium shrink-0 text-xl max-md:pb-4">002</p>
+          <p className="font-medium shrink-0 text-xl">Final Solution</p>
+          <h2 className="font-light text-lightgray basis-1/3">
+            Evolving the group listening experience by introducing improved
+            functionality and features for all Spotify users
+          </h2>
+        </div>
       </section>
     </main>
   );
