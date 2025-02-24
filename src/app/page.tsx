ffe3fd7ef14ad6036850ef43195b55008f6fe322
andy duong <andyduong510@gmail.com>
2025-02-23 16:14:12 -0800

    homepage small ui changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,17 @@ import ProjectCard from "@/components/ProjectCard";
 export default function Page() {
   return (
     <main className="flex-col min-h-screen">
-      <section className="landing px-8 lg:px-24 py-24 flex flex-col gap-4 select-none transition-[padding] duration-300 ease-in-out">
+      <section className="landing px-8 pt-[12rem] md:pt-64 flex flex-col gap-4 select-none lg:px-24 transition-[padding] duration-300 ease-in-out">
         <h1 className="text-8xl font-medium">Hello</h1>
-        <h2 className="text-4xl">I'm Andy Duong</h2>
-        <h2 className="text-medium font-light text-lightgray">
-          UX Designer based in the Bay Area, creating meaningful narratives
-          driven by my passion for music and art. Scroll down to explore
-        </h2>
+        <div className="flex flex-col gap-2">
+          <h2 className="text-2xl">I'm Andy Duong</h2>
+          <h2 className="text-medium font-light text-lightgray text-xl">
+            UX Designer based in the Bay Area, creating meaningful narratives
+            driven by my passion for music and art. Scroll down to explore
+          </h2>
+        </div>
       </section>
-      <section className="project-grid flex flex-wrap gap-6 px-8 py-10">
+      <section className="project-grid flex flex-wrap gap-6 px-8 lg:px-24 py-10">
         <div className="w-full xl:w-[calc(33.333%-1rem)] lg:w-[calc(50%-0.75rem)] transition-[width] duration-300 ease-in-out">
           <ProjectCard
             title="Readeezy AI Reading Companion"
