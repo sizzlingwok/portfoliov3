4ebd10af20388e177a9d392c1a64f7ddda5b6944
andy duong <andyduong510@gmail.com>
2025-03-06 00:59:18 -0800

    404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,6 @@
 export default function NotFound() {
   return (
-    <div className="absolute inset-0 flex items-center justify-center">
+    <div className="absolute inset-0 flex items-center justify-center -z-10">
       <div className="text-center select-none flex flex-col gap-2">
         <h1 className="text-8xl font-medium">404</h1>
         <p className="text-xl text-lightgray dark:text-dark-lightgray">
