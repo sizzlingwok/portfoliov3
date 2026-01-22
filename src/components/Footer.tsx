c0de4bc7711b1e7a50e11e5776be8841a9a968ce
andy duong <andyduong510@gmail.com>
2026-01-21 17:28:29 -0800

    resume update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="w-full py-8 px-8 lg:px-24 transition-[padding] duration-300 ease-in-out relative ">
       <div className="mx-auto flex ">
         <p className="text-[15px] text-lightgray dark:text-dark-lightgray select-none">
-          © 2025 | Andy Duong
+          © 2026 | Andy Duong
         </p>
       </div>
     </footer>
